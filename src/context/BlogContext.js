1006e87ff9824bcc093e03d1d23ad10ffc89e2ef
Morshed <morshedalamustc22@gmail.com>
2019-11-02 11:46:50 +0600

    add Reducer for manage State

--- src/context/BlogContext.js
+++ src/context/BlogContext.js
@@ -1,14 +1,23 @@
-import React,{useState} from 'react';
-import {View,StyleSheet, Text} from 'react-native';
+import React, {useReducer} from 'react';
+import {View, StyleSheet, Text} from 'react-native';
 
 const BlogContext = React.createContext();
-
-export const BlogProvider = ({children})=>{
-    const [blogPosts, setBlogPosts] = useState([]);
-    const addBlogPost = ()=>{
-        setBlogPosts([...blogPosts, {title: `Blog Post #${blogPosts.length + 1}`}])
+const blogReducer = (state, action) => {
+    switch (action.type) {
+        case 'add_blogpost':
+            return [...state, {title: `Blog Post #${state.length + 1}`}];
+        default:
+            return state;
     }
-    return <BlogContext.Provider value={{data:blogPosts, addBlogPost}}>
+};
+
+export const BlogProvider = ({children}) => {
+    const [blogPosts, dispatch] = useReducer(blogReducer, []);
+
+    const addBlogPost = () => {
+        dispatch({type: 'add_blogpost'});
+    };
+    return <BlogContext.Provider value={{data: blogPosts, addBlogPost}}>
         {children}
     </BlogContext.Provider>
 };
